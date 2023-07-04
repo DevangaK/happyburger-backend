5633b8243d04ae7deefb8d8b17e8107a00b5255f
OshiniWathsala1999 <oshini199905@gmail.com>
2023-07-04 16:52:20 +0530

    Add Catelog Service New

---
+++ catelog-service/src/main/java/com/example/catelogservice/CatelogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.catelogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatelogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatelogServiceApplication.class, args);
+    }
+
+}
